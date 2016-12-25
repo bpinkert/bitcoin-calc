49a45666c6ff2deeb818c2dd1fd19ed93fe377c8
Benjamin Pinkert <bpinkert@gmail.com>
2016-12-25 14:29:24 -0500

    all methods working
    added self attributes, got methods working

--- bitcoin-calc.py
+++ bitcoin-calc.py
@@ -5,10 +5,45 @@ import json
 class BitcoinWorker:
 	def __init__(self):
 		self.difficulty = get_difficulty()
-		#self.hashrate = get_net_hashrate()
-		#self.dailyelec = daily_watt_cost()
-		#self.poolcosts = get_poolcosts()
-		#self.exrate = get_exchangerate() 
+		self.hashrate = ""
+		self.wattage = "" 
+		self.wattcost = ""		
+		self.dailycost = ""		
+#self.exchrate = get_exchangerate()
+	#ask for information first
+	def ask_poolcosts(self):
+		poolcosts = raw_input('Input percentage paid to pool maintainer in decimal notation ex .20 as 20%\n')
+		percent = float(poolcosts) * 100
+		ipercent = int(poolcosts)
+		return percent
+	def ask_worker_hashrate(self):
+		hashrate = raw_input('Input machine hash rate in GH/s\n')
+		flhashrate = float(hashrate)
+		self.hashrate = flhashrate
+		return flhashrate
+	def ask_worker_wattage(self):
+		wattage = raw_input('Input machine power usage in Watts\n')
+		flwattage = float(wattage)
+		self.wattage = flwattage
+		return flwattage
+	def ask_wattcost(self):
+		wattcosts = raw_input('Input electricty costs per kW in cents as float ex. 7.28\n')
+		flwattcosts = float(wattcosts)
+		self.wattcost = flwattcosts
+		return flwattcosts
+	def daily_watt_cost(self):
+		usage_in_kw = self.ask_wattage() / 1000
+		daily_kwh_cost = ask_wattcost() * 24
+		daily_cost = float(usage_in_kw) * float(daily_kwh_cost)
+		self.dailycost = daily_cost
+		return daily_cost
+	def daily_hashrate(self):
+		# do we need this?
+		d = get_difficulty()
+		h = ask_worker_hashrate()
+		sec_genhash = (d * 2**32) / h 
+		day_hashrate = float(84000) / float(sec_genhash)
+		return day_hashrate
 	#static methods here for organization
 	@staticmethod
 	def get_difficulty():
@@ -17,47 +52,17 @@ class BitcoinWorker:
 		dj = json.loads(udiff)
 		return difficulty
 	@staticmethod
-	def daily_watt_cost():
-		usage_in_kw = get_wattage() / 1000
-		daily_kwh_cost = get_wattcost() * 24
-		daily_cost = float(usage_in_kw) * float(daily_kwh_cost)
-		return daily_cost
-	@staticmethod
-	def daily_hashrate():
-		d = get_difficulty()
-		h = ask_hashrate()
-		sec_genhash = (d * 2**32) / h 
-		day_hashrate = float(84000) / float(sec_genhash)
-		return day_hashrate
-	@staticmethod
 	def get_net_hashrate():
 		h = requests.get('https://blockchain.info/q/hashrate')
 		ht = h.text.encode('utf8')
 		hj = json.loads(ht)
-		net_hashrate = float(hj)
-		return net_hashrate
+		hashrate = float(hj)
+		return hashrate
 	@staticmethod
 	def get_exchangerate():
 		rate = requests.get('https://blockchain.info/ticker')
 		urate = rate.text
 		j = json.loads(urate)
 		us = j['USD']
-		usrate = us['last']
-		return usrate
-	def ask_poolcosts():
-		poolcosts = raw_input('Input percentage paid to pool maintainer in decimal notation ex .20 as 20%\n')
-		percent = float(poolcosts) * 100
-		ipercent = int(poolcosts)
-		return percent
-	def ask_hashrate():
-		hashrate = raw_input('Input machine hash rate in GH/s\n')
-		flhashrate = float(hashrate)
-		return hashrate
-	def ask_wattage():
-		wattage = raw_input('Input machine power usage in Watts\n')
-		flwattage = float(wattage)
-		return flwattage
-	def ask_wattcost():
-		wattcosts = raw_input('Input electricty costs per kW in cents as float ex. 7.28\n')
-		flwattcosts = float(wattcosts)
-		return flwattcosts
+		exchrate = us['last']
+		return exchrate
